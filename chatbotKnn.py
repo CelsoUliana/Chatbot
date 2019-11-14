fe6180f5d30cb2a8f0c37e756fc5d7ddb100bd41
Celso Antonio Uliana Junior <32890738+CelsoUliana@users.noreply.github.com>
2019-11-14 18:20:01 -0300

    Knn e análise

--- chatbotKnn.py
+++ chatbotKnn.py
@@ -1,98 +1,87 @@
-'''
-    Celso Antonio Uliana Junior - Nov 2019
-'''
+###
+#   Celso Antonio Uliana Junior - Nov 2019
+###
 
 
-'''
-    Este trabalho consiste em um chatbot para uma hamburgueria.
-'''
 
-'''
-    Importação de dependencias.
-'''
+#   Este trabalho consiste em um chatbot para uma hamburgueria.
+
+
+
+#   Importação de dependencias.
 import csv
 import pandas
 import numpy as np
+import param_grid as params
+
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 
 x = []
 y = []
 
+#   Leitura de parâmetros do modulo.
+p = params.knn()
 
-'''
-    Leitura de dados.
-'''
+#   Leitura do dataset.
 with open('dataset.csv', newline='', encoding='utf-8') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
         y.append(row['inten'])
         x.append(row['texto'])
 
-'''
-    Transformar os dados em BOW.
-'''
+#   Transformar os dados em BOW(bag of words).
 vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 0.5, strip_accents = 'unicode')
 x = vectorizer.fit_transform(x)
 
-
-'''
-    Instanciar o leave one outs.
-'''
+#   Instanciar o leave one outs.
 loo = LeaveOneOut()
 loo.get_n_splits(x)
 
+#   Instanciar um np array e y predito.
 yi = np.array(y)
-
 y_pred = []
 
-'''
-    Usando leave one outs para treinar o KNN
-'''
+#   Faz o hypertuning dos modelo.
+grid_search = GridSearchCV(KNeighborsClassifier(), p, scoring = 'accuracy', cv = 5, iid = False)
+grid_search.fit(x, yi)
+
+print('grid search:')
+print(grid_search.best_params_)
+#print(grid_search.best_estimator_)
+#print(grid_search.best_index_)
+print(grid_search.best_score_)
+
+#   Usando leave one outs para treinar o KNN
 for train_index, test_index in loo.split(x):
-    #print("TRAIN:", train_index, "TEST:", test_index)
     X_train, X_test = x[train_index], x[test_index]
     y_train, y_test = yi[train_index], yi[test_index]
-    #print(X_train, X_test, y_train, y_test)
-    model = KNeighborsClassifier(n_neighbors = 4)
+    model = KNeighborsClassifier(algorithm = 'auto', leaf_size = 30, metric = 'euclidean',
+                     metric_params = None, n_jobs = None, n_neighbors = 3, p = 2,
+                     weights = 'uniform')
+
     model.fit(X_train, y_train)
     val = model.predict(X_test)
     y_pred.append(val)
 
-
-'''
-    Metricas
-'''
-print('Acuracia')
+#   Metricas do modelo KNN.
+print('\nAcuracia')
 print(accuracy_score(yi, y_pred))
 print(accuracy_score(yi, y_pred, normalize=False))
 
-print('precisao')
+print('\nprecisao')
 print(precision_score(yi, y_pred, average='macro'))
 print(precision_score(yi, y_pred, average='micro'))
 print(precision_score(yi, y_pred, average='weighted'))
 
-print('recall score')
+print('\nrecall score')
 print(recall_score(yi, y_pred, average='macro'))
 print(recall_score(yi, y_pred, average='micro'))
 print(recall_score(yi, y_pred, average='weighted'))
-
-'''
-model = KNeighborsClassifier(n_neighbors = 1)
-model.fit(x,yi)
-'''
-
-'''
-acurracy = cross_val_score(model, x, yi, scoring = 'accuracy', cv=3)
-print(acurracy)
-'''
-
-text = 'Quero um xbacon'
-inst = vectorizer.transform([text])
-print(model.predict(inst))
---
+++ param_grid.py
@@ -0,0 +1,17 @@
+###
+#   Celso Antonio Uliana Junior - Nov 2019
+###
+
+def knn():
+    return {    'metric': ('euclidean', 'minkowski', 'manhattan' ), 
+                'n_neighbors':(1,3,5,7,9,11,13,15,17,19),
+                'weights': ('uniform', 'distance')
+            }
+
+tree = {
+
+}
+
+regressao = {
+
+}
