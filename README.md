e37c32d7a41c0aae466a91b9351e77ef19eb852a
Celso Antonio Uliana Junior <32890738+CelsoUliana@users.noreply.github.com>
2019-11-17 00:53:05 -0300

    Chatbot, fase 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trabalho de IA 2019 - 2
+Trabalho de IA, fazer um chatbot.
+
+Andamento do trabalho pode ser acompanhado pelo link:
+
+https://github.com/CelsoUliana/Chatbot
+
+Melhor modelo entre os 3 foi o de regressão logistica.
+de acordo com o comparativo no comparação.txt (que uso os prints de cada um dos chatbot(modelos))
--- chatbotRL.py
+++ chatbotRL.py
@@ -15,14 +15,14 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 x = []
 y = []
 
 #   Leitura de parâmetros do modulo.
-p = params.knn()
+p = params.regressao()
 
 #   Leitura do dataset.
 with open('dataset.csv', newline='', encoding='utf-8') as csv_file:
@@ -43,23 +43,24 @@ loo.get_n_splits(x)
 yi = np.array(y)
 y_pred = []
 
-#   Usando leave one outs para treinar o KNN
+#   Usando leave one outs para treinar a RL
 for train_index, test_index in loo.split(x):
     X_train, X_test = x[train_index], x[test_index]
     y_train, y_test = yi[train_index], yi[test_index]
 
     #   Fine-tuning dos parâmetros
-    grid_search = GridSearchCV(KNeighborsClassifier(), p, scoring = 'accuracy', cv = 5, iid = False)
+    grid_search = GridSearchCV(LogisticRegression(), p, scoring = 'accuracy', cv = 5, iid = False)
     grid_search.fit(X_train, y_train)
 
     #   Modelo
-    model = KNeighborsClassifier(metric=grid_search.best_params_['metric'], n_neighbors=grid_search.best_params_['n_neighbors'], weights=grid_search.best_params_['weights'])
+    model = LogisticRegression(random_state = grid_search.best_params_['random_state'], C = grid_search.best_params_['C'], multi_class = grid_search.best_params_['multi_class'],
+    solver = grid_search.best_params_['solver'], class_weight = grid_search.best_params_['class_weight'])
 
     model.fit(X_train, y_train)
     val = model.predict(X_test)
     y_pred.append(val)
 
-#   Metricas do modelo KNN.
+#   Metricas do modelo RL.
 print('\nAcuracia')
 print(accuracy_score(yi, y_pred))
 print(accuracy_score(yi, y_pred, normalize=False))
